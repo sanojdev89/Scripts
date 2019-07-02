2d22293172101ebbaae5514c6aaedaa12991cace
sanojdev89 <40886563+sanojdev89@users.noreply.github.com>
2019-07-02 12:05:29 +0530

    cretaed new postgres file

---
+++ test_postgres.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PGPASSWORD=docker
+containerID=$( docker ps | grep pg-docker | awk '{print $1}' )
+reg_stat=$( docker inspect --format="{{.State.Running}}" $containerID )
+echo "container ID is $containerID"
+echo "my postgres running status is $reg_stat"
+if [  "$reg_stat" = "true" ]
+then
+        echo "postgres container is up and running"
+                docker exec -it pg-docker  psql -U postgres -c "SELECT pg_reload_conf();" >> test.txt
+
+                if [ -s test.txt ]
+                then
+                        echo "Postgres service is up and running"
+                else
+                        echo "postgres service is down"
+                fi
+
+else
+        echo "postgres container is down"
+fi
